6fdaf5edf6df1e5cef13f360cd73f5c11a785b7f
David Whittemore <git@xpollen8.com>
2021-06-08 16:11:04 -0700

    dw: don't need api

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,33 @@
 import Head from 'next/head'
 
-export async function getServerSideProps({ query = {} }) {
-	const id = query.id || 0;
-	const { results, errors } = await (require('../lib/fetch_data'))(`/api/swipe/${id}`);
+import HTDB from 'HTDBjs';
+
+let htdb;
+
+export async function getServerSideProps({ query: { path = [], id = 0 } = {} }) {
+	const intId = parseInt(id);
+	if (!htdb) {
+		htdb = new HTDB(0);
+	}
+
+	htdb.prerender = () => {
+		// override htdb file values
+		const pageMax = Object.keys(htdb.defines).filter(f => {
+			const [ page, num ] = f.split('page');
+			return num === '0' || parseInt(num);
+		}).length;
+		if (intId < 1 || intId >= pageMax) { intId = pageMax; }
+		htdb.define({ name: 'pageMax', body: `${pageMax}` });
+		htdb.define({ name: 'nextUp', body: `/?id=${(intId < 1) ? pageMax : intId - 1}` });
+		htdb.define({ name: 'thisNum', body: `${intId - 1}` });
+	}
+	console.log("SERVING", intId);
 	return {
-		props: { id, ...results }
+		props: { body: await htdb.render() }
 	}
 }
 
-const Swipe = ({ id, body }) => (
+const Swipe = ({ body }) => (
 	<div className="container">
 		<Head>
 			<title>Sideswipe</title>
