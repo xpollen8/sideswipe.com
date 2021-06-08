eaa1e1db29f0cfc699a57867ec855dc25392cf22
David Whittemore <git@xpollen8.com>
2021-06-08 13:37:07 -0700

    dw: using new HTDBjs, added prerender

--- pages/api/swipe/[id].js
+++ pages/api/swipe/[id].js
@@ -2,36 +2,24 @@ import HTDB from 'HTDBjs';
 
 let htdb;
 
-const defs = `
-#include	buh
-#
-#	hello!
-# 
-#live if (blah)
-#live	endif
-
-#define	index.html hello world
-#live if (blah)
-	hello blah
-#live	endif
-`;
-
 const fetchSwipe = async (id) => {
 	let intId = parseInt(id);
 	if (!htdb) {
-		htdb = new HTDB('site.htdb', 0);
-		await htdb.load();
+		htdb = new HTDB(0);
+		//await htdb.load();
 	}
-	// override htdb file values
-	const pageMax = Object.keys(htdb.defines).filter(f => {
-		const [ page, num ] = f.split('page');
-		return num === '0' || parseInt(num);
-	}).length;
-	if (intId < 1 || intId >= pageMax) { intId = pageMax; }
-	htdb.define({ name: 'pageMax', body: `${pageMax}` });
+	htdb.prerender = () => {
+		// override htdb file values
+		const pageMax = Object.keys(htdb.defines).filter(f => {
+			const [ page, num ] = f.split('page');
+			return num === '0' || parseInt(num);
+		}).length;
+		if (intId < 1 || intId >= pageMax) { intId = pageMax; }
+		htdb.define({ name: 'pageMax', body: `${pageMax}` });
+		htdb.define({ name: 'nextUp', body: `/?id=${(intId < 1) ? pageMax : intId - 1}` });
+		htdb.define({ name: 'thisNum', body: `${intId - 1}` });
+	}
 	console.log("SERVING", intId);
-	htdb.define({ name: 'nextUp', body: `/?id=${(intId < 1) ? pageMax : intId - 1}` });
-	htdb.define({ name: 'thisNum', body: `${intId - 1}` });
 	return { body: await htdb.render() }
 }
 
